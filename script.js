838b969108be77d6b32b1b265a5881c85e1239c4
Nguyen Son <ngso.ts@gmail.com>
2021-12-12 10:00:12 +0700

    finish form styling

---
+++ script.js
@@ -0,0 +1,12 @@
+// import mapboxgl from 'mapbox-gl';
+
+// const token = process.env.MAP_BOX_ACCESS_TOKEN;
+
+// mapboxgl.accessToken = token;
+
+// const map = new mapboxgl.Map({
+//   container: 'map',
+//   style: 'mapbox://styles/mapbox/streets-v11',
+//   center: [-74.5, 40],
+//   zoom: 9,
+// });
