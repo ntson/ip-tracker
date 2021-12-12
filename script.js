02aabf3445acbccda5f751acb9feec3bf8ab816c
Nguyen Son <ngso.ts@gmail.com>
2021-12-12 10:41:19 +0700

    finish

--- script.js
+++ script.js
@@ -1,12 +1,54 @@
-// import mapboxgl from 'mapbox-gl';
+import mapboxgl from 'mapbox-gl';
 
-// const token = process.env.MAP_BOX_ACCESS_TOKEN;
+const token = process.env.MAP_BOX_ACCESS_TOKEN;
 
-// mapboxgl.accessToken = token;
+const baseURL = 'http://ip-api.com/json/';
 
-// const map = new mapboxgl.Map({
-//   container: 'map',
-//   style: 'mapbox://styles/mapbox/streets-v11',
-//   center: [-74.5, 40],
-//   zoom: 9,
-// });
+mapboxgl.accessToken = token;
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [-74.5, 40],
+  zoom: 10,
+});
+
+const marker = new mapboxgl.Marker().setLngLat([-74.5, 40]).addTo(map);
+
+const displayError = function (message) {
+  const error = document.createElement('span');
+  error.classList.add('error');
+  error.textContent = message;
+
+  const headerEl = document.querySelector('.header');
+  headerEl.appendChild(error);
+};
+
+const form = document.querySelector('.header-form');
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+  const { ip } = Object.fromEntries(formData);
+
+  fetch(`${baseURL}${ip}`)
+    .then((res) => {
+      return res.json();
+    })
+    .then((location) => {
+      map.flyTo({
+        center: [location.lon, location.lat],
+        essential: true,
+      });
+
+      marker.remove();
+
+      new mapboxgl.Marker().setLngLat([location.lon, location.lat]).addTo(map);
+
+      const input = document.getElementById('ip');
+      input.value = '';
+    })
+    .catch((error) => {
+      displayError(error.message);
+    });
+});
